6a49aaf6fff4c51d564eb97ec58c0833aa062e4f
WenchangLu <wlu2@ncsu.edu>
2022-05-21 19:12:59 -0400

    add plotting dos, rms, totalE and error in E

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 import os
+import io
+from plot_dos import *
+from check_convergence import *
 st.title('RMG Postprocess User Interface')
 st.markdown("Welcome to use rmg package! This interface will help you plot some resutls from RMG")
 st.write('<style>div.row-widget.stRadio > div{flex-direction:row;justify-content: left}<style>',
         unsafe_allow_html=True)
 
+
 st.sidebar.markdown("")
 st.sidebar.markdown("")
 st.sidebar.markdown("")
@@ -14,4 +19,15 @@ st.sidebar.markdown("")
 st.sidebar.markdown("")
 st.sidebar.markdown("")
 
-uploaded_file = st.sidebar.file_uploader("Choose a file to plot")
+what_to_plot = st.sidebar.radio("what to plot", ['DOS', 'Check Convergence'])
+if what_to_plot == 'DOS':
+    plot_dos()
+elif what_to_plot == 'Check Convergence':
+    what_to_check = st.sidebar.radio("RMS or Total energy", ['RMS', 'error in Total Energy', 'Total Energy'])
+    if what_to_check == 'RMS':
+        plot_rms()
+    if what_to_check == 'error in Total Energy':
+        plot_deltaE()
+    if what_to_check == 'Total Energy':
+        plot_totE()
+
