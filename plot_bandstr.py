6b32b28f5d62c1255f3777ea42faf07bfd05fbaf
WenchangLu <wlu2@ncsu.edu>
2022-05-22 18:32:11 -0400

    forget plot_rho.py file

---
+++ plot_bandstr.py
@@ -0,0 +1,64 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import io
+def plot_band():
+
+    file_op = st.sidebar.radio("Here is the band structure of primitive diamond, upload others by youself", ["primitive diamond cell", "upload a .dat file")
+    data =""
+    if file_op == "primitive diamond cell":
+        with open("example_diamod2/", "r") as f:
+            data = f.readlines()
+    else:
+        upload_file = st.sidebar.file_uploader("Upload dos_tot_spin*.dat to  plot", type='dat')
+        if upload_file:
+            data = upload_file.readlines()
+    #print(data)
+        
+    if data:    
+        x = []
+        y = []
+        for line in data:
+            x.append(float(line.split()[0]))
+            y.append(float(line.split()[1]))
+
+        x_max = max(x)
+        x_min = min(x)
+        y_max = max(y)
+        y_min = min(y)
+        x_range = st.sidebar.slider("Select x range", x_min, x_max, (x_min, x_max))
+        y_range = st.sidebar.slider("Select y range", y_min, y_max, (y_min, y_max))
+
+        #plt.style.use('_mpl-gallery-nogrid')
+        fig, ax = plt.subplots()
+        ax.plot(x,y,linewidth=2.0, color="red")
+        ax.set_xlim(x_range[0], x_range[1])
+        ax.set_ylim(y_range[0], y_range[1])
+        plt.title("Electronic Density of States", fontsize=20)
+        #plt.get_frame().set_linewidth(1.0)
+        ax.set_xlabel('E (eV)', fontsize=20)
+        ax.set_ylabel('Density of States', fontsize=20)
+        ax.tick_params(direction='in', length=6, width=2, colors='black')
+
+        ax.tick_params(axis='x', labelsize=20)
+        ax.tick_params(axis='y', labelsize=20)
+
+        fn = 'tot_dos.png'
+        img = io.BytesIO()
+        plt.tight_layout()
+        plt.figure(dpi=600)
+        for axis in ['top','bottom','left','right']:
+            ax.spines[axis].set_linewidth(2.0)
+        quality_dpi = st.sidebar.number_input("dpi of image to be saved", 600)    
+        plt.savefig(img, format='png', dpi =quality_dpi)
+
+        btn = st.sidebar.download_button(
+           label="Download image",
+           data=img,
+           file_name=fn,
+           mime="image/png"
+        )
+
+        st.pyplot(fig)
