fa4d0954b982f0fc8aeaa059c6f83aa101112d70
WenchangLu <wlu2@ncsu.edu>
2022-05-23 18:07:27 -0400

    determine the min and max of rho from data

--- plot_rho.py
+++ plot_rho.py
@@ -1,22 +1,50 @@
 import streamlit as st
 import py3Dmol
+import numpy as np
 from stmol import showmol
 def plot_rho():
 
     upload_file = st.sidebar.file_uploader("Upload a .cube format file to  plot", type='cube')
     if upload_file:
         rho_data = upload_file.read().decode('utf-8')
-        rho_data_lines = upload_file.readlines()
+        rho_data_lines = rho_data.split("\n")
         for i in range(len(rho_data_lines)):
             rho_data_lines[i] = rho_data_lines[i].decode('utf-8')
     else:
         with open("C60/density.cube", "r") as f:
             rho_data = f.read()
-            rho_data_lines = f.readlines()
+            rho_data_lines = rho_data.split("\n")
+
+
+    num_atoms = int(rho_data_lines[2].split()[0])
+    real_or_complex = int(rho_data_lines[2].split()[4])
+    nx = int(rho_data_lines[3].split()[0])
+    ny = int(rho_data_lines[4].split()[0])
+    nz = int(rho_data_lines[5].split()[0])
+    vol_data_start_line = num_atoms + 6
+    num_lines_per_z = (nz*real_or_complex)//6 +1
+
+    rho_3d = np.ndarray([nx,ny,nz*real_or_complex], dtype = float)
+    for ix in range(nx):
+        for iy in range(ny):
+            line_index = (ix * ny + iy) * num_lines_per_z + vol_data_start_line
+            z_count = 0
+            for iz_line in range(line_index, line_index+num_lines_per_z):
+                if z_count == nz * real_or_complex:
+                    break
+                tem_str = rho_data_lines[iz_line].split()
+                for s in tem_str:
+                    rho_3d[ix][iy][z_count] = float(s)
+                    z_count += 1
+
+    rho_1d = rho_3d.reshape(nx*ny*nz, real_or_complex)
+    rho_max = np.max(rho_1d, axis=0)
+    rho_min = np.min(rho_1d, axis=0)
+    rho_ave = 0.5 * (float(rho_max[0]) + float(rho_min[0]))
 
     spin = st.sidebar.checkbox('Spin', value = False)
     iso_color = st.sidebar.radio("isosurface color", ['yellow', 'blue', 'red','gold'])
-    isosurface_value = st.sidebar.slider("isosurface value", 0.0, 0.2, 0.1)
+    isosurface_value = st.sidebar.slider("isosurface value", float(rho_min[0]), float(rho_max[0]), rho_ave)
     #rho_view.setBackgroundColor(bcolor)
 
     rho_view = py3Dmol.view()
