a7086417629f0eb0c7ef93b08ff6dc249931c53d
WenchangLu <wlu2@ncsu.edu>
2023-06-06 21:02:36 -0800

    disable view structure

--- main.py
+++ main.py
@@ -21,7 +21,8 @@ st.sidebar.markdown("")
 st.sidebar.markdown("")
 st.sidebar.markdown("")
 
-what_to_plot = st.sidebar.radio("what to plot", ['Charge Density', 'DOS', 'Band Structure', 'Check Convergence', 'view xyz', 'STM'])
+#what_to_plot = st.sidebar.radio("what to plot", ['Charge Density', 'DOS', 'Band Structure', 'Check Convergence', 'view xyz', 'STM'])
+what_to_plot = st.sidebar.radio("what to plot", ['Charge Density', 'DOS', 'Band Structure', 'Check Convergence', 'STM'])
 if what_to_plot == 'DOS':
     plot_dos()
 elif what_to_plot == 'Check Convergence':
