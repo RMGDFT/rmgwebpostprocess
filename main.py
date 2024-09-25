4d7982c007aef1ad82324418a836de7c764562be
WenchangLu <wlu2@ncsu.edu>
2024-09-25 10:35:52 -0400

    add more package for stmol

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from check_convergence import *
 from plot_rho import *
 from plot_stm_slowversion import *
 from plot_bandstr import *
-from view_xyz import *
+#from view_xyz import *
 st.title('RMG Postprocess User Interface')
 st.markdown("Welcome to use rmg package! This interface will help you plot some results from RMG")
 st.write('<style>div.row-widget.stRadio > div{flex-direction:row;justify-content: left}<style>',
@@ -38,6 +38,7 @@ elif what_to_plot == "Charge Density":
 elif what_to_plot == "Band Structure":
     plot_band()
 elif what_to_plot == "view xyz":
-    view_xyz()
+    st.markdown("not able to view strucutre now"
+    #view_xyz()
 elif what_to_plot == "STM":
     plot_stm()
